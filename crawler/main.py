b72849c64232148c382d577ebe3f4d5149cd9ffb
Francesco Coppola <fg.coppola@outlook.com>
2025-06-11 12:39:18 +0200

    Added test python crawler to start the pipeline

---
+++ crawler/main.py
@@ -0,0 +1,35 @@
+import hashlib, json, os, socket, shutil
+from time import sleep
+
+PDF_PATH = "/tmp/2-DECO-SUPERMERCATI-13-22-MAGGIO.pdf"
+SHARED_FOLDER = "/data/"
+LOGSTASH_HOST = os.getenv("LOGSTASH_HOST", "localhost")
+LOGSTASH_PORT = int(os.getenv("LOGSTASH_PORT", 5044))
+
+def sha256sum(filepath):
+    with open(filepath, "rb") as f:
+        return hashlib.sha256(f.read()).hexdigest()
+
+def send_to_logstash(payload):
+    print("Connecting to:", LOGSTASH_HOST, LOGSTASH_PORT)
+    with socket.create_connection((LOGSTASH_HOST, LOGSTASH_PORT)) as sock:
+        sock.sendall((json.dumps(payload) + "\n").encode("utf-8"))
+
+def simulate_download():
+    shutil.copy(PDF_PATH, SHARED_FOLDER)
+
+def main():
+    simulate_download()
+    checksum = sha256sum(PDF_PATH)
+    filename = os.path.basename(PDF_PATH)
+    payload = {
+        "filename": filename,
+        "path": SHARED_FOLDER + filename,
+        "checksum": checksum
+    }
+    print("Sending metadata:", payload)
+    sleep(1)
+    send_to_logstash(payload)
+
+if __name__ == "__main__":
+    main()
