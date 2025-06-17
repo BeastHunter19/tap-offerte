b2d631931cffdbb43edfcbdee23ea077621dcdbe
Francesco Coppola <fg.coppola@outlook.com>
2025-06-17 20:57:50 +0200

    Changed index creation script to create any number of indexes

--- elasticsearch/init-index.sh
+++ elasticsearch/init-index.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-
 set -e
 
 ES_HOST=${ES_HOST:-http://elasticsearch:9200}
-INDEX_NAME=${INDEX_NAME:-offers}
 
-if curl -s -f -o /dev/null "$ES_HOST/$INDEX_NAME"; then
-  echo "Index '$INDEX_NAME' already exists. Skipping creation."
-else
-  echo "Creating index '$INDEX_NAME'..."
-  curl -X PUT "$ES_HOST/$INDEX_NAME" \
-    -H "Content-Type: application/json" \
-    -d @/index.json
-fi
+for index_file in /elasticsearch/index.*.json; do
+  index_name=$(basename "$index_file" | sed 's/^index\.//' | sed 's/\.json$//')
+
+  echo "Checking if index '$index_name' exists..."
+
+  if curl -s -f -o /dev/null "$ES_HOST/$index_name"; then
+    echo "Index '$index_name' already exists. Skipping."
+  else
+    echo "Creating index '$index_name' from file '$index_file'..."
+    curl -X PUT "$ES_HOST/$index_name" \
+      -H "Content-Type: application/json" \
+      -d @"$index_file"
+  fi
+done
